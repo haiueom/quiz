587ed334511c9dc8bb4fb16c2c150fd859e43ab5
Ilham Taufiq <65374686+iamilham@users.noreply.github.com>
2022-02-15 11:25:51 +0700

    Update end.js

--- js/end.js
+++ js/end.js
@@ -25,5 +25,5 @@ saveHighScore = (e) => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
+    window.location.assign('/index.html');
 };
